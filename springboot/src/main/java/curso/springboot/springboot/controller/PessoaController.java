906a947899f87077e1a9c002b7ab307374705780
marcus aurelio <marcusaurelio743@gmail.com>
2023-11-25 18:18:50 -0300

    implementado metodo de editar e salvar

--- springboot/src/main/java/curso/springboot/springboot/controller/PessoaController.java
+++ springboot/src/main/java/curso/springboot/springboot/controller/PessoaController.java
@@ -1,8 +1,11 @@
 package curso.springboot.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,11 +20,14 @@ public class PessoaController {
 	
 	
 	@GetMapping(value = "/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	public ModelAndView inicio() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
 	}
 	
-	@PostMapping(value = "/salvarpessoa")
+	@PostMapping(value = "*/salvarpessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 		repository.save(pessoa);
 		
@@ -31,10 +37,19 @@ public class PessoaController {
 		
 		//relaciona no objeto modelAndView o atibuto pessoas com a lista de pessoas 
 		andView.addObject("pessoas", pessoasIt);
-		
+		andView.addObject("pessoaobj", new Pessoa());
 		return andView;
 	}
 	
+	@GetMapping(value = "/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa")Long id) {
+		
+		Optional<Pessoa> pessoa = repository.findById(id);
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoaobj", pessoa.get());
+		return andView;
+	}
 	@GetMapping(value = "/listapessoas")
 	public ModelAndView pessoas() {
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
@@ -43,6 +58,7 @@ public class PessoaController {
 		
 		//relaciona no objeto modelAndView o atibuto pessoas com a lista de pessoas 
 		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
 		
 		return andView;
 		
